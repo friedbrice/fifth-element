d95ba9cac25616fb540f511dfc1b7e936f34c205
Daniel Brice <danielbrice@gmail.com>
2020-05-02 00:38:24 -0700

    TODO

--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,10 @@ import Imports
 
 import Effect.Console (log)
 
+-- First order of business is getting a web page up and running
+-- For an example, see
+-- http://holdenlee.github.io/blog/posts/programming/purescript/snake-in-purescript.html
+
 main :: Effect Unit
 main = do
   log "🍝"
